00366703012a94c21f4ba17e482da65af42e8141
DarrenEdelbert <82514500+DarrenEdelbert@users.noreply.github.com>
2026-04-10 19:28:09 +0530

    Add: max-water-container - submission-4

---
+++ Data Structures & Algorithms/max-water-container/submission-4.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        # DID THIS PROBLEM ON MY OWN I'M GOATED
+        # first i calculate the volume of the current 2 bars
+        # height and distance matters -> whichever bar is smaller, we move forward/backward from that
+        # then calculate the volumes for all bars and return the biggest
+        l,r = 0, len(heights)-1
+        vol = 0
+        while l<r:
+            vol = max(vol, (min(heights[l],heights[r]) * (r-l)))
+            print("L,R,V", heights[l], heights[r], vol)
+            if heights[l]<heights[r]:
+                l+=1
+            elif heights[l]>heights[r]:
+                r-=1
+            else: 
+                r-=1 # doesnt matter if i increase l or decrease r
+        return vol
